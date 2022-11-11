a8a79e90938f04d89988d2681776a092922ea626
Selim GORMUS <siberfx@users.noreply.github.com>
2022-11-11 17:11:07 +0300

    migration file path updated

--- src/ShoppingcartServiceProvider.php
+++ src/ShoppingcartServiceProvider.php
@@ -33,9 +33,7 @@ class ShoppingcartServiceProvider extends ServiceProvider
             // Publish the migration
             $timestamp = date('Y_m_d_His', time());
 
-            $this->publishes([
-                __DIR__.'/../database/migrations/0000_00_00_000000_create_shoppingcart_table.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_table.php'),
-            ], 'migrations');
+            $this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')], 'migrations');
         }
     }
 }
